b8858138185f6c63e849c6d894cf7cec77833ea9
Macarena Alvear <macarena.alvear1901@alumnos.ubiobio.cl>
2025-10-12 22:02:31 -0300

    T1FSPL-43 User Register View

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import TripsPage from './pages/TripsPage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="/trips" element={<TripsPage />} />
         </Route>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
