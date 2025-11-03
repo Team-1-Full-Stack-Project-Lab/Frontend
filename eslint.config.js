744225665594e4f158c1f64f0394289af0ed0ef6
Carlos Badilla <carlosbadillac@outlook.com>
2025-11-03 00:07:08 -0300

    feat: add ESLint rules for unused variables to improve code quality

--- eslint.config.js
+++ eslint.config.js
@@ -21,5 +21,16 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
 ])
